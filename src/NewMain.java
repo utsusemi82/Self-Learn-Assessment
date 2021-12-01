64efa7496b328120f42d8de73f1b15bbb6609af7
utsusemi82 <>
2021-12-01 22:51:09 +0800

    new main

---
+++ src/NewMain.java
@@ -0,0 +1,34 @@
+
+import java.util.Random;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Random r= new Random();
+        
+        int x=5;
+        int y=12;
+        char c= 0xcad;
+        
+        System.out.println(c);
+        System.out.println(x++*y/(8-x));
+        
+        
+        }
+        
+    }
+    
+
